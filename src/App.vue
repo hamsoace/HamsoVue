48f32b4553ed647420507b8a49ae1d367c54f63d
hamsoace <hamsoace@gmail.com>
2021-08-04 00:05:31 +0300

    conditional rendering #1

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <div>{{greet}} {{name}}</div>
-  <div v-html="career"></div>
-  <!-- <div v-html="hack"></div> -->
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 class="underline" v-bind:class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldOut ? 'sold-out' : 'new'">Sold Out Movie</h2>
-  <h2 :class="['new', 'promoted']">Newly Promoted Movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array Conditional Movie</h2>
-  <h2 :class="{
-    promoted: isPromoted,
-    new: !isSoldOut,
-  }">Object Conditional Movie</h2>
-  <h2 :style="{
-    color: highlightColor,
-    'font-size': headerSize + 'px',
-    padding: '20px',
-  }">Inline Style</h2>
-  <h2 :style="headerStyleObject">Style Object</h2>
+  <h2 v-if="num === 0">The Number is Zero</h2>
+  <h2 v-else-if="num < 0">The Number is Negative</h2>
+  <h2 v-else-if="num > 0">The Number is Positive</h2>
+  <h2 v-else>Not a Number</h2>
 
+  <div v-if="display">
+    <h2>Hamisi</h2>
+    <h2>Software Dev</h2>
+    <h2>Vue</h2>
+  </div>
+  
 </template>
 
 <script>
@@ -29,23 +19,8 @@ export default {
   name: 'App',
   data(){
     return{
-      greet: "Hello",
-      name: "HamsoAce",
-      career: "<b>Software Dev</b>",
-      // hack: `<a href="github.com/hamsoace" onclick="alert('You have been hacked!')">Win a prize!</a>`,
-      headingId: 'heading',
-      isDisabled: false,
-      status: 'success',
-      isPromoted: true,
-      isSoldOut: true,
-      highlightColor: 'blue',
-      headerSize: 30,
-      headerStyleObject: {
-        color: 'yellow',
-        fontSize: '25px',
-        padding: '30px',
-
-      }
+      num: -10,
+      display: true,
     }
   }
  
@@ -60,22 +35,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.promoted{
-  font-style: italic;
-}
-
-.underline{
-  text-decoration: underline;
-}
-
-.sold-out{
-  color: red;
-}
-
-.new{
-  color: aqua;
 }
 
 </style>
