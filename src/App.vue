ffceab6955bdf0d20ae29ca925a948d2ffa2deb1
hamsoace <hamsoace@gmail.com>
2021-08-03 23:12:34 +0300

    binding attributes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>{{greet}} {{name}}</div>
   <div v-html="career"></div>
   <div v-html="hack"></div>
+  <button v-bind:disabled="isDisabled">Bind</button>
 
 </template>
 
@@ -16,6 +17,8 @@ export default {
       name: "HamsoAce",
       career: "<b>Software Dev</b>",
       hack: `<a href="github.com/hamsoace" onclick="alert('You have been hacked!')">Win a prize!</a>`,
+      headingId: 'heading',
+      isDisabled: false,
     }
   }
  
